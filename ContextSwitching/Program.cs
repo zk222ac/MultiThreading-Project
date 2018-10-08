d564f258367109779bd528fdb6f52c809154149f
Zuhair Haroon Khan <zk222ac@gmail.com>
2018-10-08 22:48:03 +0200

    hi

--- ContextSwitching/Program.cs
+++ ContextSwitching/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ContextSwitching
@@ -10,6 +11,41 @@ namespace ContextSwitching
     {
         static void Main(string[] args)
         {
+            Thread thread1 = new Thread(DoSomethingforThread1);
+            Thread thread2 = new Thread(DoSomethingforThread2);
+            thread1.Name = "thread1...........................";
+            Console.WriteLine(thread1.Name);
+            // worker thread has many but main thread is only one
+            thread1.Start();
+            thread2.Start();
+            // Main Thread start here 
+            Thread.CurrentThread.Name = "Main Thread................";
+            Console.WriteLine(Thread.CurrentThread.Name);
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("Main Thread:" + i);
+            }
+
+            Console.ReadKey();
+
+        }
+
+        private static void DoSomethingforThread1()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("Worker Thread 1:" + i);
+            }
+        }
+        private static void DoSomethingforThread2()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("Worker Thread 2:" + i);
+            }
         }
     }
 }
